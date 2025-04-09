1c8ad673881480b01e4be9a48e770e4b6e3dba63
Анастасия Кор <idealizm@mail.ru>
2025-04-09 17:54:08 +0300

    step 7

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ https://asciinema.org/a/AYb2zpUC6a2bwczIEeJqssfZS
 
 ### brain-calc
 https://asciinema.org/a/oyKBkuaj3RymBK8s7jeFU1IR8
+
+### brain-gcd
+https://asciinema.org/a/fNPIIaGPj8jeA3Moj9ipjGgd7
---
+++ games/gcd.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+import {gameLogik} from '../src/index.js';
+
+const gameDescription = 'Find the greatest common divisor of given numbers.';
+
+const ask = () => {
+    let number1 = Math.round(Math.random()*100,0);
+    let number2 = Math.round(Math.random()*100,0);
+    let rightAns = 1;
+
+    const qwestion = readlineSync.question(`Question: ${number1} ${number2}\nYour answer: `);
+    const answer = Number(qwestion);
+
+    for (let i = 1; i <= number1; i++) {
+        if ((number1% i === 0) && (number2% i === 0)) {
+            rightAns = i;
+        };  
+    };
+
+    return [answer, rightAns];
+};
+
+export default  () => {
+        gameLogik(ask, gameDescription);
+};
